eee30eb81576e11e8f7397a2d072e15b5769bd7d
unknown <u77709633@gmail.com>
2025-07-20 07:45:29 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig = {
   },
   output: 'export', // Required for static HTML export
   basePath: '/finflip', // Match your GitHub repo name
+  assetPrefix: '/finflip/',
   trailingSlash: true,  // Required for static HTML pages
 };
 
