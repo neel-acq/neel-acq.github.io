46df9b6d1dd3d7dc1dbd680aa5bd995ed1b0c45a
unknown <unknown@gmail.com>
2025-07-24 13:05:06 +0530

    Add generated sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,6 @@
 module.exports = {
     siteUrl: "https://finflip.vercel.app", // ✅ Replace with your live domain
     generateRobotsTxt: true, // ✅ also generates robots.txt
-    generateIndexSitemap: false, // optional: disable sitemap-index if site is small
+    // generateIndexSitemap: false, // optional: disable sitemap-index if site is small
     sitemapSize: 5000,
 };
