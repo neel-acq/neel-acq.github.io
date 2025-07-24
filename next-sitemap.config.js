b9fea0f3d2254f2793714c41394f674e30e13098
unknown <unknown@gmail.com>
2025-07-24 12:54:38 +0530

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://finflip.vercel.app", // ✅ Replace with your live domain
+    generateRobotsTxt: true, // ✅ also generates robots.txt
+    generateIndexSitemap: false, // optional: disable sitemap-index if site is small
+    sitemapSize: 5000,
+};
